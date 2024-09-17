01e692533424a28f6513f9ee02e428a05ab0d9d6
shafiq115 <shafiq@discoverpilgrim.com>
2024-09-17 15:02:35 +0530

    Update Customer_append.sql

--- data_models/Customers/Customer_append.sql
+++ data_models/Customers/Customer_append.sql
@@ -3,11 +3,12 @@ MERGE INTO `shopify-pubsub-project.Shopify_staging.Customers` AS target
 
 USING (
   SELECT
-   _airbyte_extracted_at,
+
+  _airbyte_extracted_at,
   accepts_marketing as Customer_accepts_marketing,
   tax_exempt as Customer_tax_exempt,
   verified_email as Customer_verified_email,
-  id as Customer_id,
+  CAST(id AS STRING) as Customer_id,
   phone as Customer_phone,
   last_name as Customer_last_name,
   first_name as Customer_first_name,
@@ -17,26 +18,26 @@ USING (
   tags as Customer_tags,
   updated_at as Customer_updated_at,
   created_at as Customer_created_at,
-  accepts_marketing_updated_at as Customer_accepts_marketing_updated_at,
+  CAST(JSON_EXTRACT_SCALAR(accepts_marketing_updated_at) AS TIMESTAMP) as Customer_accepts_marketing_updated_at,
 
-  JSON_EXTRACT(default_address, '$.address1') AS Customer_address1,
-  JSON_EXTRACT(default_address, '$.address2') AS Customer_address2,
-  JSON_EXTRACT(default_address, '$.city') AS Customer_city,
-  JSON_EXTRACT(default_address, '$.country_code') AS Customer_country_code,
-  JSON_EXTRACT(default_address, '$.country_name') AS Customer_country_name,
-  JSON_EXTRACT(default_address, '$.id') AS Address_id,
-  JSON_EXTRACT(default_address, '$.province') AS Customer_province,
-  JSON_EXTRACT(default_address, '$.province_code') AS Customer_province_code,
-  JSON_EXTRACT(default_address, '$.zip') AS Customer_zip,
+  JSON_EXTRACT_SCALAR(default_address, '$.address1') AS Customer_address1,
+  JSON_EXTRACT_SCALAR(default_address, '$.address2') AS Customer_address2,
+  JSON_EXTRACT_SCALAR(default_address, '$.city') AS Customer_city,
+  JSON_EXTRACT_SCALAR(default_address, '$.country_code') AS Customer_country_code,
+  JSON_EXTRACT_SCALAR(default_address, '$.country_name') AS Customer_country_name,
+  JSON_EXTRACT_SCALAR(default_address, '$.id') AS Address_id,
+  JSON_EXTRACT_SCALAR(default_address, '$.province') AS Customer_province,
+  JSON_EXTRACT_SCALAR(default_address, '$.province_code') AS Customer_province_code,
+  JSON_EXTRACT_SCALAR(default_address, '$.zip') AS Customer_zip,
 
-  JSON_EXTRACT(sms_marketing_consent, '$.consent_collected_from') AS sms_consent_collected_from,
-  JSON_EXTRACT(sms_marketing_consent, '$.consent_updated_at') AS sms_consent_updated_at,
-  JSON_EXTRACT(sms_marketing_consent, '$.opt_in_level') AS sms_consent_opt_in_level,
-  JSON_EXTRACT(sms_marketing_consent, '$.state') AS sms_consent_state,
+  JSON_EXTRACT_SCALAR(sms_marketing_consent, '$.consent_collected_from') AS sms_consent_collected_from,
+  CAST(JSON_EXTRACT_SCALAR(sms_marketing_consent, '$.consent_updated_at') AS TIMESTAMP) AS sms_consent_updated_at,
+  JSON_EXTRACT_SCALAR(sms_marketing_consent, '$.opt_in_level') AS sms_consent_opt_in_level,
+  JSON_EXTRACT_SCALAR(sms_marketing_consent, '$.state') AS sms_consent_state,
 
-  JSON_EXTRACT(email_marketing_consent, '$.consent_updated_at') AS email_consent_updated_at,
-  JSON_EXTRACT(email_marketing_consent, '$.opt_in_level') AS email_consent_opt_in_level,
-  JSON_EXTRACT(email_marketing_consent, '$.state') AS email_consent_state,
+  CAST(JSON_EXTRACT_SCALAR(email_marketing_consent, '$.consent_updated_at') AS TIMESTAMP) AS email_consent_updated_at,
+  JSON_EXTRACT_SCALAR(email_marketing_consent, '$.opt_in_level') AS email_consent_opt_in_level,
+  JSON_EXTRACT_SCALAR(email_marketing_consent, '$.state') AS email_consent_state,
 
   FROM `shopify-pubsub-project.airbyte711.customers`
     WHERE date(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 1 DAY)
@@ -140,3 +141,4 @@ source.email_consent_updated_at,
 source.email_consent_opt_in_level,
 source.email_consent_state
   )
+
