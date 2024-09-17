d25b1a4fb4c515c9ff3e918ce2d613c302e20a3a
shafiq115 <shafiq@discoverpilgrim.com>
2024-09-17 15:05:50 +0530

    Update Fulfillment_append.sql

--- data_models/Fulfillment/Fulfillment_append.sql
+++ data_models/Fulfillment/Fulfillment_append.sql
@@ -3,22 +3,22 @@ MERGE INTO `shopify-pubsub-project.Shopify_staging.Fulfillments` AS target
 
 USING (
   SELECT
-    _airbyte_extracted_at as _airbyte_extracted_at,
+   _airbyte_extracted_at as _airbyte_extracted_at,
     shop_url as fulfillment_shop_url,
-    status as fulfillment_status,A
+    status as fulfillment_status,
     service as fulfillment_service,
-    location_id as fulfillment_location_id,
+    CAST(location_id AS STRING) as fulfillment_location_id,
     shipment_status as fulfillment_shipment_status,
     tracking_company as fulfillment_tracking_company,
     updated_at as fulfillment_updated_at,
     tracking_number as fulfillment_tracking_number,
     tracking_url as fulfillment_tracking_url,
     created_at as fulfillment_created_at,
-    order_id as fulfillment_order_id,
+    CAST(order_id AS STRING) as fulfillment_order_id,
     name as fulfillment_name,
-    id as fulfillment_id,
-    tracking_urls[0] as fulfillment_tracking_urls,
-    tracking_numbers[0] as fulfillment_tracking_numbers,
+    CAST(id AS STRING) as fulfillment_id,
+    CAST(JSON_EXTRACT_SCALAR(tracking_urls[0]) AS STRING) as fulfillment_tracking_urls,
+    CAST(JSON_EXTRACT_SCALAR(tracking_numbers[0]) AS STRING) as fulfillment_tracking_numbers,
   FROM `shopify-pubsub-project.airbyte711.fulfillments`
   WHERE date(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 1 DAY)
  
@@ -80,3 +80,7 @@ source.fulfillment_tracking_urls,
 source.fulfillment_tracking_numbers
 
   )
+
+
+
+
