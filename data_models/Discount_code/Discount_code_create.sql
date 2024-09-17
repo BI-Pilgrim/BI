5a22aef57786a79abed4fd7a00bcc54a23d9315e
shafiq115 <shafiq@discoverpilgrim.com>
2024-09-17 15:04:07 +0530

    Update Discount_code_create.sql

--- data_models/Discount_code/Discount_code_create.sql
+++ data_models/Discount_code/Discount_code_create.sql
@@ -1,4 +1,6 @@
 
+-- current_datetime("Asia/Kolkata"),
+
 CREATE or replace TABLE `shopify-pubsub-project.Shopify_staging.Discount_Code`
 PARTITION BY DATE_TRUNC(discount_created_at,day)
 CLUSTER BY disocunt_code
@@ -9,7 +11,7 @@ OPTIONS(
  AS 
 SELECT 
 _airbyte_extracted_at as _airbyte_extracted_at,
-id as discount_id,
+CAST(id AS STRING) as discount_id,
 code as disocunt_code,
 summary as discount_summary,
 shop_url as shop_url,
@@ -17,5 +19,5 @@ created_at as discount_created_at,
 updated_at as discount_updated_at,
 usage_count as discount_usage_count,
 discount_type,
-price_rule_id,
+CAST(price_rule_id AS STRING) AS price_rule_id,
  FROM  `shopify-pubsub-project.airbyte711.discount_codes`
