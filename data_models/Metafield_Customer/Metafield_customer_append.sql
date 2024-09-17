998ffacfb49ed21ef392fce28f2840ccc635d811
shafiq115 <shafiq@discoverpilgrim.com>
2024-09-17 15:06:40 +0530

    Update Metafield_customer_append.sql

--- data_models/Metafield_Customer/Metafield_customer_append.sql
+++ data_models/Metafield_Customer/Metafield_customer_append.sql
@@ -3,17 +3,17 @@ MERGE INTO `shopify-pubsub-project.Shopify_staging.Metafield_customers` AS targe
 
 USING (
   SELECT
-    _airbyte_extracted_at as _airbyte_extracted_at,
-    id as metafield_id,
+    _airbyte_extracted_at,
+    CAST(id AS STRING) as metafield_id,
     key as metafield_key,
     type as metafield_type,
     value as metafield_value,
-    owner_id as customer_id,
+    CAST(owner_id AS STRING) as customer_id,
     shop_url as metafield_shop_url,
     namespace as metafield_namespace,
     created_at as customer_created_at,
     updated_at as customer_updated_at,
-    owner_resource as metafield_owner_resource
+    owner_resource as metafield_owner_resource,
 
   FROM `shopify-pubsub-project.airbyte711.metafield_customers`
   WHERE date(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 1 DAY)
@@ -58,3 +58,7 @@ source.customer_created_at,
 source.customer_updated_at,
 metafield_owner_resource
   )
+
+
+
+
