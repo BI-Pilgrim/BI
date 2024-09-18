add4dd9c1876d2239155dacd8e4e9b447790eace
shafiq115 <shafiq@discoverpilgrim.com>
2024-09-18 13:00:41 +0530

    Update Draft_order_item_append.sql

--- data_models/Draft_Order_items/Draft_order_item_append.sql
+++ data_models/Draft_Order_items/Draft_order_item_append.sql
@@ -1,5 +1,5 @@
 
-MERGE INTO `shopify-pubsub-project.Shopify_staging.Draft_Orders_Items` AS target
+MERGE INTO `shopify-pubsub-project.Shopify_staging.Draft_Order_items` AS target
 
 USING (
   select 
