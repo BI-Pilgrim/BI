26279b58ef45bd1a939b4363cd860b6a38b8d544
shafiq115 <shafiq@discoverpilgrim.com>
2024-09-23 16:48:07 +0530

    Granularity checked and duplicates handled

--- data_models/Refund_Order_item/Refund_Order_item_create.sql
+++ data_models/Refund_Order_item/Refund_Order_item_create.sql
@@ -1,9 +1,16 @@
 
-MERGE INTO `shopify-pubsub-project.Shopify_staging.Refund_Order_items` AS target
+CREATE or replace TABLE `shopify-pubsub-project.Shopify_staging.Refund_Order_items`
 
-USING (
-  select 
-  distinct
+PARTITION BY DATE_TRUNC(Order_created_at,day)
+-- CLUSTER BY 
+
+OPTIONS(
+ description = "Refund Orders item table is partitioned on Order date at day level",
+ require_partition_filter = FALSE
+ )
+ AS 
+ select 
+ distinct
 _airbyte_extracted_at,
 Order_created_at,
 Order_name,
@@ -17,20 +24,27 @@ order_adjustments_amount,
 order_adjustments_id,
 order_adjustments_kind,
 order_adjustments_reason,
+
 CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.id') AS FLOAT64) as item_refund_id,
 CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.id') AS FLOAT64) as order_item_id,
+
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.name') as refund_item_name,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.price') as refund_item_price,
+
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.product_id') as refund_product_id,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.sku') as refund_SKU_code,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.sku') as refund_item_quantity,
+
+
 CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.tax_lines[0].price') AS FLOAT64) as item_tax_price1,
 CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.tax_lines[0].rate') AS FLOAT64) as item_tax_rate1,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.tax_lines[0].title') as item_tax_type1,
 CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.tax_lines[1].price') AS FLOAT64) as item_tax_price2,
 CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.tax_lines[1].rate') AS FLOAT64) as item_tax_rate2,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.tax_lines[1].title') as item_tax_type2,
+
 CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.total_discount') AS FLOAT64) as refund_item_discount_price,
+
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.title') as refund_product_tile,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.variant_id') as refund_variant_id,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.variant_title') as refund_variant_title,
@@ -74,109 +88,3 @@ FROM  `shopify-pubsub-project.airbyte711.orders`
 
 ), UNNEST (refund_item_flat) AS FULL_FLAT
 )
-  WHERE date(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 10 DAY)
- 
- ) AS source
-ON target.item_refund_id = source.item_refund_id
-WHEN MATCHED AND source._airbyte_extracted_at > target._airbyte_extracted_at THEN UPDATE SET
-
-target._airbyte_extracted_at = source._airbyte_extracted_at,
-target.Order_created_at = source.Order_created_at,
-target.Order_name = source.Order_name,
-target.customer_id = source.customer_id,
-target.Refund_created_at = source.Refund_created_at,
-target.Refund_id = source.Refund_id,
-target.Refund_note = source.Refund_note,
-target.Refund_Order_id = source.Refund_Order_id,
-target.Refund_processed_at = source.Refund_processed_at,
-target.order_adjustments_amount = source.order_adjustments_amount,
-target.order_adjustments_id = source.order_adjustments_id,
-target.order_adjustments_kind = source.order_adjustments_kind,
-target.order_adjustments_reason = source.order_adjustments_reason,
-target.item_refund_id = source.item_refund_id,
-target.order_item_id = source.order_item_id,
-target.refund_item_name = source.refund_item_name,
-target.refund_item_price = source.refund_item_price,
-target.refund_product_id = source.refund_product_id,
-target.refund_SKU_code = source.refund_SKU_code,
-target.refund_item_quantity = source.refund_item_quantity,
-target.item_tax_price1 = source.item_tax_price1,
-target.item_tax_rate1 = source.item_tax_rate1,
-target.item_tax_type1 = source.item_tax_type1,
-target.item_tax_price2 = source.item_tax_price2,
-target.item_tax_rate2 = source.item_tax_rate2,
-target.item_tax_type2 = source.item_tax_type2,
-target.refund_item_discount_price = source.refund_item_discount_price,
-target.refund_product_tile = source.refund_product_tile,
-target.refund_variant_id = source.refund_variant_id,
-target.refund_variant_title = source.refund_variant_title
-
-WHEN NOT MATCHED THEN INSERT (
-_airbyte_extracted_at,
-Order_created_at,
-Order_name,
-customer_id,
-Refund_created_at,
-Refund_id,
-Refund_note,
-Refund_Order_id,
-Refund_processed_at,
-order_adjustments_amount,
-order_adjustments_id,
-order_adjustments_kind,
-order_adjustments_reason,
-item_refund_id,
-order_item_id,
-refund_item_name,
-refund_item_price,
-refund_product_id,
-refund_SKU_code,
-refund_item_quantity,
-item_tax_price1,
-item_tax_rate1,
-item_tax_type1,
-item_tax_price2,
-item_tax_rate2,
-item_tax_type2,
-refund_item_discount_price,
-refund_product_tile,
-refund_variant_id,
-refund_variant_title
-   )
-  VALUES (
-source._airbyte_extracted_at,
-source.Order_created_at,
-source.Order_name,
-source.customer_id,
-source.Refund_created_at,
-source.Refund_id,
-source.Refund_note,
-source.Refund_Order_id,
-source.Refund_processed_at,
-source.order_adjustments_amount,
-source.order_adjustments_id,
-source.order_adjustments_kind,
-source.order_adjustments_reason,
-source.item_refund_id,
-source.order_item_id,
-source.refund_item_name,
-source.refund_item_price,
-source.refund_product_id,
-source.refund_SKU_code,
-source.refund_item_quantity,
-source.item_tax_price1,
-source.item_tax_rate1,
-source.item_tax_type1,
-source.item_tax_price2,
-source.item_tax_rate2,
-source.item_tax_type2,
-source.refund_item_discount_price,
-source.refund_product_tile,
-source.refund_variant_id,
-source.refund_variant_title
-
-  )
-
-
-
-
