c3e4a95fcbf8edd101a50d04d7df8e5d99d41101
shafiq115 <shafiq@discoverpilgrim.com>
2024-09-23 16:50:16 +0530

    Granularity checked and duplicates handled

--- Data_Model_Master/Customer_Master/Customer_Master_Append.sql
+++ Data_Model_Master/Customer_Master/Customer_Master_Append.sql
@@ -4,6 +4,7 @@ MERGE INTO `shopify-pubsub-project.Shopify_Production.Customer_Master` AS target
 USING (
   
 SELECT
+    distinct
     C._airbyte_extracted_at,
     C.Customer_accepts_marketing,
     C.Customer_tax_exempt,
@@ -36,22 +37,21 @@ SELECT
     C.email_consent_opt_in_level,
     C.email_consent_state,
     MC._airbyte_extracted_at AS MC_airbyte_extracted_at,
-    MC.metafield_id,
-    MC.metafield_key,
-    MC.metafield_type,
-    MC.metafield_value,
+    MC.Gender_field,
+    MC.Personalization_field,
+    MC.Concerns_field,
     MC.customer_id AS MC_customer_id,
     MC.metafield_shop_url,
-    MC.metafield_namespace,
     MC.customer_created_at AS MC_customer_created_at,
     MC.customer_updated_at AS MC_customer_updated_at,
-    MC.metafield_owner_resource,
+    MC.metafield_owner_resource
+
   FROM
     `shopify-pubsub-project.Shopify_staging.Customers` AS C
     LEFT OUTER JOIN `shopify-pubsub-project.Shopify_staging.Metafield_customers` AS MC 
     ON C.Customer_id = MC.customer_id
  
-    WHERE date(C._airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 1 DAY)
+    WHERE date(C._airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 10 DAY)
  
  ) AS source
 ON target.Customer_id = source.Customer_id
@@ -88,16 +88,15 @@ target.email_consent_updated_at = source.email_consent_updated_at,
 target.email_consent_opt_in_level = source.email_consent_opt_in_level,
 target.email_consent_state = source.email_consent_state,
 target.MC_airbyte_extracted_at = source.MC_airbyte_extracted_at,
-target.metafield_id = source.metafield_id,
-target.metafield_key = source.metafield_key,
-target.metafield_type = source.metafield_type,
-target.metafield_value = source.metafield_value,
+target.Gender_field = source.Gender_field,
+target.Personalization_field = source.Personalization_field,
+target.Concerns_field = source.Concerns_field,
 target.MC_customer_id = source.MC_customer_id,
 target.metafield_shop_url = source.metafield_shop_url,
-target.metafield_namespace = source.metafield_namespace,
 target.MC_customer_created_at = source.MC_customer_created_at,
 target.MC_customer_updated_at = source.MC_customer_updated_at,
 target.metafield_owner_resource = source.metafield_owner_resource
+
 
 WHEN NOT MATCHED THEN INSERT (
 _airbyte_extracted_at,
@@ -132,13 +131,11 @@ email_consent_updated_at,
 email_consent_opt_in_level,
 email_consent_state,
 MC_airbyte_extracted_at,
-metafield_id,
-metafield_key,
-metafield_type,
-metafield_value,
+Gender_field,
+Personalization_field,
+Concerns_field,
 MC_customer_id,
 metafield_shop_url,
-metafield_namespace,
 MC_customer_created_at,
 MC_customer_updated_at,
 metafield_owner_resource
@@ -177,14 +174,15 @@ source.email_consent_updated_at,
 source.email_consent_opt_in_level,
 source.email_consent_state,
 source.MC_airbyte_extracted_at,
-source.metafield_id,
-source.metafield_key,
-source.metafield_type,
-source.metafield_value,
+source.Gender_field,
+source.Personalization_field,
+source.Concerns_field,
 source.MC_customer_id,
 source.metafield_shop_url,
-source.metafield_namespace,
 source.MC_customer_created_at,
 source.MC_customer_updated_at,
 source.metafield_owner_resource
+
   )
+
+
