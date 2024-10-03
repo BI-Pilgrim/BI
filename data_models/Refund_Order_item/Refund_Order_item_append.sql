abb2ed53e4b6c5cb435f53c91e1f4e0c9e155536
shafiq115 <shafiq@discoverpilgrim.com>
2024-10-03 14:48:28 +0530

    Update Refund_Order_item_append.sql

--- data_models/Refund_Order_item/Refund_Order_item_append.sql
+++ data_models/Refund_Order_item/Refund_Order_item_append.sql
@@ -17,8 +17,8 @@ order_adjustments_amount,
 order_adjustments_id,
 order_adjustments_kind,
 order_adjustments_reason,
-CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.id') AS FLOAT64) as item_refund_id,
-CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.id') AS FLOAT64) as order_item_id,
+CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.id') AS STRING) as item_refund_id,
+CAST(JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.id') AS STRING) as order_item_id,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.name') as refund_item_name,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.price') as refund_item_price,
 JSON_EXTRACT_SCALAR(Full_FLAT,'$.line_item.product_id') as refund_product_id,
@@ -78,6 +78,7 @@ FROM  `shopify-pubsub-project.airbyte711.orders`
  
  ) AS source
 ON target.item_refund_id = source.item_refund_id
+and target.refund_variant_id = source.refund_variant_id
 WHEN MATCHED AND source._airbyte_extracted_at > target._airbyte_extracted_at THEN UPDATE SET
 
 target._airbyte_extracted_at = source._airbyte_extracted_at,
