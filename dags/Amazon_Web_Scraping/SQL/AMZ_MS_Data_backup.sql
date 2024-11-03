6d0e52543976d4b09cd745b64bec25542803d00a
shafiq115 <shafiq@discoverpilgrim.com>
2024-11-03 22:51:32 +0530

    Solved insertion issue
    since the column name were not in sequence of amazon and current table insertion was not possible so columns were made in sequence.

--- dags/Amazon_Web_Scraping/SQL/AMZ_MS_Data_backup.sql
+++ dags/Amazon_Web_Scraping/SQL/AMZ_MS_Data_backup.sql
@@ -1,12 +1,53 @@
--- Before starting with the new data append first add the missing SKU from the Amazon coming data to the same table w.r.t previous month data 
-
 INSERT INTO `shopify-pubsub-project.Amazon_Market_Sizing.AMZ_Market_Sizing`
 select 
- CM.* 
+CM.Product_Title,
+CM.Parent_ASIN,
+CM.Child_ASIN,
+CM.Product_URL,
+CM.Ratings,
+CM.MRP_Price,
+CM.Per_100ml_price,
+CM.Selling_Price,
+CM.Unit_Sold,
+CM.Size_of_SKU,
+CM.Brand_value,
+CM.Best_Seller_in_Beauty,
+CM.Best_Seller_in_Category,
+CM.Scent_type,
+CM.Skin_type,
+CM.Benefits,
+CM.Item_form,
+CM.Item_weight,
+CM.Active_ingredient,
+CM.Net_volume,
+CM.Skin_tone,
+CM.Item_volume,
+CM.Pack_size,
+CM.SPF_factor,
+CM.Hair_type,
+CM.Material_type_free,
+CM.Recomended_for,
+CM.Reviews_Summary,
+CM.Max_Unit_Sold,
+CM.Revenue,
+CM.Max_Revenue,
+CM.Category,
+CM.Benefits_type,
+CM.Active_ingredient_type,
+CM.Material_type_free_type,
+CM.Scent_type_type,
+CM.Hair_type_type,
+CM.Recomended_for_type,
+CM.Item_weight_in_gm,
+CM.Net_volume_in_ml,
+CM.Item_volume_in_ml,
+CM.SPF_factor_type,
+CM.Date_MS,
+
 from `shopify-pubsub-project.Amazon_Market_Sizing.AMZ_current_month_MS` as CM
 left join `shopify-pubsub-project.Amazon_Market_Sizing.AMZ_Market_Sizing` as ND 
-on CM.Child_ASIN = ND.Child_ASIN
-where ND.Child_ASIN is null;
+on CM.Child_ASIN = ND.ASIN
+where ND.ASIN is null;
 
 -- drop the current table
 DROP TABLE `shopify-pubsub-project.Amazon_Market_Sizing.AMZ_current_month_MS`;
