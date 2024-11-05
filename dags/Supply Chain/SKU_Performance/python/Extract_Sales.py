aa98af2ed272bc691af5d2ba982a96e5087b62cf
KajalRay-12 <kajal.ray@discoverpilgrim.com>
2024-11-05 17:26:14 +0530

    Update Extract_Sales.py

--- dags/Supply Chain/SKU_Performance/python/Extract_Sales.py
+++ dags/Supply Chain/SKU_Performance/python/Extract_Sales.py
@@ -18,7 +18,7 @@ if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
-        flow = InstalledAppFlow.from_client_secrets_file('gcs/dags/D2C/SKU_Performance/python/credentials.json', SCOPES)
+        flow = InstalledAppFlow.from_client_secrets_file('gcs/dags/Supply Chain/SKU_Performance/python/credentials.json', SCOPES)
         creds = flow.run_local_server(port=0)
     with open('token.json', 'w') as token:
         token.write(creds.to_json())
