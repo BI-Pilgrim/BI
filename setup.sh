31a7d70efcb3c147f1016dfd0f45550dcdb5d5c1
Uday Krishna <udaykrishna@users.noreply.github.com>
2024-11-25 16:37:48 +0530

    change line end

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 airflow db migrate
 airflow users create --role Admin --username admin --email admin --firstname admin --lastname admin --password admin
-
 # airflow webserver --port 8080 & airflow scheduler && fg
 airflow standalone
