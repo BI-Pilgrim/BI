13a807ea28c33fdd0c6c478dc4a98609aaa9cae4
KajalRay-12 <kajal.ray@discoverpilgrim.com>
2024-12-04 10:58:11 +0530

    Update Draft_order_create.sql

--- dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Draft_orders/Draft_order_create.sql
+++ dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Draft_orders/Draft_order_create.sql
@@ -1,37 +1,66 @@
 
-
-CREATE or replace TABLE `shopify-pubsub-project.Shopify_staging.Draft_Orders`
-PARTITION BY DATE_TRUNC(draft_order_created_at,day)
-CLUSTER BY draft_order_status
+CREATE or replace TABLE `shopify-pubsub-project.Data_Warehouse_Shopify_Staging.Draft_orders`
+PARTITION BY DATE_TRUNC(Draft_order_created_at,day)
+CLUSTER BY Draft_order_id
 OPTIONS(
- description = "Draft Order table is partitioned on draft order created at",
+ description = "Draft Order table is partitioned on  Draft_order created at",
  require_partition_filter = False
  )
  AS 
 SELECT 
-distinct 
+distinct
 _airbyte_extracted_at,
-status as draft_order_status,
-payment_terms as draft_order_payment_terms,
-CAST(total_tax AS FLOAT64) as draft_order_total_tax,
-CAST(total_price AS FLOAT64) as draft_order_total_price,
-tags as draft_order_tags,
-note as draft_order_note,
-email as draft_order_email,
-completed_at as draft_order_completed_at,
-CAST(order_id AS STRING) as draft_order_id,
-created_at as draft_order_created_at,
-updated_at as draft_order_updated_at,
-CAST(id AS STRING) as draft_id,
-CAST(name AS STRING) as draft_order_name,
-invoice_url as draft_order_invoice_url,
+id,
+name as Draft_order_name,
+note as Draft_order_note,
+tags as Draft_order_tag,
+status as Draft_order_status,
+currency,
+CAST(JSON_EXTRACT_SCALAR(customer, "$.default_address.name") AS STRING) AS Customer_name,
+CAST(JSON_EXTRACT_SCALAR(customer, "$.default_address.phone") AS STRING) AS Customer_phone,
+CAST(JSON_EXTRACT_SCALAR(customer, "$.id") AS INT64) AS Customer_id,
+CAST(JSON_EXTRACT_SCALAR(customer, "$.email") AS STRING) AS Customer_email,
+order_id as Draft_order_id,
+shop_url,
+CAST(total_tax AS FLOAT64) AS total_tax,
+created_at AS Draft_order_created_at,
 
-CAST(JSON_EXTRACT_SCALAR(customer, '$.id') AS STRING) AS customer_id,
+CAST(JSON_EXTRACT_SCALAR(item, "$.name") AS STRING) AS Product_name,
+CAST(JSON_EXTRACT_SCALAR(item, "$.sku") AS STRING) AS Product_SKU,
+CAST(JSON_EXTRACT_SCALAR(item, "$.variant_id") AS INT64) AS Product_variant_id,
+CAST(JSON_EXTRACT_SCALAR(item, "$.price") AS INT64) AS Product_price,
+CAST(JSON_EXTRACT_SCALAR(JSON_EXTRACT(item, "$.tax_lines[0]"), "$.price") AS FLOAT64) AS Product_tax_amount,
+tax_exempt,
+updated_at AS Draft_order_updated_at,
+invoice_url,
+CAST(total_price AS FLOAT64) AS Draft_order_amount,
+completed_at AS Draft_order_completed_at,
 
-CAST(JSON_EXTRACT_SCALAR(applied_discount, '$.amount') AS FLOAT64) AS discount_amount,
-CAST(JSON_EXTRACT_SCALAR(applied_discount, '$.title') AS STRING) AS discount_title,
-CAST(JSON_EXTRACT_SCALAR(applied_discount, '$.value') AS FLOAT64) AS discount_value,
-CAST(JSON_EXTRACT_SCALAR(applied_discount, '$.value_type') AS STRING) AS discount_value_type,
+CAST(REGEXP_EXTRACT(REPLACE(payment_terms, "'", '"'), r'"payment_terms_id":\s*(\d+)') AS INT64) AS Payment_terms_id,
+CAST(REGEXP_EXTRACT(REPLACE(payment_terms, "'", '"'), r'"reference_id":\s*(\d+)') AS INT64) AS Payment_reference_id,
+CAST(REGEXP_EXTRACT(REPLACE(payment_terms, "'", '"'), r'"total_balance":\s*"([\d\.]+)"') AS FLOAT64) AS Payment_balance_amount,
 
-FROM  `shopify-pubsub-project.airbyte711.draft_orders`
 
+CAST(JSON_EXTRACT_SCALAR(shipping_line, "$.price") AS INT64) AS Shipping_price,
+CAST(JSON_EXTRACT_SCALAR(shipping_line, "$.title") AS STRING) AS Shipping_title,
+
+CAST(subtotal_price AS FLOAT64)	AS	Draft_order_subtotal,
+taxes_included,
+CONCAT(
+  CAST(JSON_EXTRACT_SCALAR(billing_address, "$.address1") AS STRING), 
+    ", ", 
+  CAST(JSON_EXTRACT_SCALAR(billing_address, "$.address2") AS STRING)
+  ) AS Billing_address,
+invoice_sent_at,	
+CAST(JSON_EXTRACT_SCALAR(applied_discount, "$.amount") AS STRING) AS	Discount_amount,
+CONCAT(
+  CAST(JSON_EXTRACT_SCALAR(shipping_address, "$.address1") AS STRING), 
+    ", ", 
+  CAST(JSON_EXTRACT_SCALAR(shipping_address, "$.address2") AS STRING)
+  ) AS Shipping_address,
+
+CAST(JSON_EXTRACT_SCALAR(shipping_address, "$.city") AS STRING) AS Shipping_city,
+admin_graphql_api_id
+
+from `shopify-pubsub-project.pilgrim_bi_airbyte.draft_orders`,
+UNNEST(JSON_EXTRACT_ARRAY(line_items)) AS item
