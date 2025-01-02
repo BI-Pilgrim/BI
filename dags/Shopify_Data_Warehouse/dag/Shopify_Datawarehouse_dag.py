545395fca20375e3d66d230c89e5dc6bf38af098
RwitapaM <rwitapa.mitra@discoverpilgrim.com>
2025-01-02 13:59:39 +0530

    Update Shopify_Datawarehouse_dag.py

--- dags/Shopify_Data_Warehouse/dag/Shopify_Datawarehouse_dag.py
+++ dags/Shopify_Data_Warehouse/dag/Shopify_Datawarehouse_dag.py
@@ -40,7 +40,7 @@ with DAG(
 # Abandoned Checkout Staging Table Refresh - Append
 
     # Load SQL query from file
-    with open('gcs/dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Abandoned_chekout/Abandoned_checkout_create.sql', 'r') as file:
+    with open('../dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Abandoned_chekout/Abandoned_checkout_create.sql', 'r') as file:
         sql_query_1 = file.read()
 
     append_abandoned_checkout = BigQueryInsertJobOperator(
