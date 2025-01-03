931aefa5bb026e1789c929b33b6a7e769dc56768
Uday Krishna <udaykrishna@users.noreply.github.com>
2025-01-04 00:36:39 +0530

    Add extracted_at parameter to load_data_to_bigquery for timestamp tracking

--- dags/easy_com/states/get_states.py
+++ dags/easy_com/states/get_states.py
@@ -88,10 +88,11 @@ class easyEComStatesAPI(EasyComApiConnector):
         truncate_query = f"DELETE FROM `{table_ref}` WHERE true"
         self.client.query(truncate_query).result()  # Executes the DELETE query
 
-    def load_data_to_bigquery(self, data):
+    def load_data_to_bigquery(self, data, extracted_at):
         """Load the data into BigQuery."""
         print("Loading States data to BigQuery")
         df = pd.DataFrame(data)
+        df["ee_extracted_at"] = extracted_at
         job_config = bigquery.LoadJobConfig(
             write_disposition=bigquery.WriteDisposition.WRITE_APPEND
         )
