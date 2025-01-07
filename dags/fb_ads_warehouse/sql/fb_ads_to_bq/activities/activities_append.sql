312c5a1e33ef3d24ae9e0fb7197daf0928fd5672
Omkar0893 <omkar.sadawarte@discoverpilgrim.com>
2025-01-07 22:43:44 +0530

    SQL code changes and test_DAG finalization for production

--- dags/fb_ads_warehouse/sql/fb_ads_to_bq/activities/activities_append.sql
+++ dags/fb_ads_warehouse/sql/fb_ads_to_bq/activities/activities_append.sql
@@ -1,35 +1,32 @@
 MERGE INTO `shopify-pubsub-project.Data_Warehouse_Facebook_Ads_Staging.activities` AS TARGET
-USING
-(
+USING (
   SELECT
-  _airbyte_extracted_at,
-  actor_id,
-  object_id,
-  account_id,
-  actor_name,
-  event_time,
-  event_type,
-  extra_data,
-  object_name,
-  object_type,
-  application_id,
-  application_name,
-  date_time_in_timezone,
-  translated_event_type,
-  FROM
-  (
+    _airbyte_extracted_at,
+    actor_id,
+    object_id,
+    account_id,
+    actor_name,
+    event_time,
+    event_type,
+    extra_data,
+    object_name,
+    object_type,
+    application_id,
+    application_name,
+    date_time_in_timezone,
+    translated_event_type
+  FROM (
     SELECT
       *,
-      ROW_NUMBER() OVER (PARTITION BY _airbyte_extracted_at ORDER BY _airbyte_extracted_at DESC) AS row_num
-      FROM
-        `shopify-pubsub-project.pilgrim_bi_airbyte_facebook.activities`
-      WHERE
-        DATE(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 10 DAY)
+      ROW_NUMBER() OVER (PARTITION BY object_id ORDER BY _airbyte_extracted_at DESC) AS row_num
+    FROM
+      `shopify-pubsub-project.pilgrim_bi_airbyte_facebook.activities`
+    WHERE
+      DATE(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 10 DAY)
   )
-  WHERE row_num = 1 -- Keep only the most recent row per label_id
+  WHERE row_num = 1 -- Keep only the most recent row per object_id
 ) AS SOURCE
-ON
-  TARGET.object_id = SOURCE.object_id
+ON TARGET.object_id = SOURCE.object_id
 WHEN MATCHED AND TARGET._airbyte_extracted_at > SOURCE._airbyte_extracted_at
 THEN UPDATE SET
   TARGET._airbyte_extracted_at = SOURCE._airbyte_extracted_at,
@@ -47,8 +44,7 @@ THEN UPDATE SET
   TARGET.date_time_in_timezone = SOURCE.date_time_in_timezone,
   TARGET.translated_event_type = SOURCE.translated_event_type
 WHEN NOT MATCHED
-THEN INSERT
-(
+THEN INSERT (
   _airbyte_extracted_at,
   actor_id,
   object_id,
@@ -64,8 +60,7 @@ THEN INSERT
   date_time_in_timezone,
   translated_event_type
 )
-VALUES
-(
+VALUES (
   SOURCE._airbyte_extracted_at,
   SOURCE.actor_id,
   SOURCE.object_id,
@@ -80,4 +75,4 @@ VALUES
   SOURCE.application_name,
   SOURCE.date_time_in_timezone,
   SOURCE.translated_event_type
-)
+);
