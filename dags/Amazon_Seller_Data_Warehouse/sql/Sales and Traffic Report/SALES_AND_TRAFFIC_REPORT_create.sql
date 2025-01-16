55a855d93afb83bcb40b42530bd0fc236ce4618f
KajalRay-12 <kajal.ray@discoverpilgrim.com>
2025-01-16 13:13:59 +0530

    Update SALES_AND_TRAFFIC_REPORT_create.sql

--- dags/Amazon_Seller_Data_Warehouse/sql/Sales and Traffic Report/SALES_AND_TRAFFIC_REPORT_create.sql
+++ dags/Amazon_Seller_Data_Warehouse/sql/Sales and Traffic Report/SALES_AND_TRAFFIC_REPORT_create.sql
@@ -1,28 +1,34 @@
 
 CREATE OR REPLACE TABLE `shopify-pubsub-project.Data_Warehouse_Amazon_Seller_Staging.SALES_AND_TRAFFIC_REPORT`
-PARTITION BY DATE_TRUNC(queryEndDate,DAY)
+PARTITION BY DATE_TRUNC(queryEndDate, DAY)
 CLUSTER BY parentAsin
 OPTIONS(
-  description = "sales and traffic table is partitioned on query end Date ",
+  description = "Sales and traffic table is partitioned on query end date",
   require_partition_filter = FALSE
 )
- AS 
+AS 
 SELECT  
-distinct
-_airbyte_extracted_at,
-sku,
-childAsin,
-parentAsin,
-queryEndDate,
-json_extract_scalar(trafficByAsin,'$.browserPageViews') as browser_page_view,
-json_extract_scalar(trafficByAsin,'$.browserSessions') as browser_sessions,
-json_extract_scalar(trafficByAsin,'$.mobileAppPageViews') as app_page_view,
-json_extract_scalar(trafficByAsin,'$.pageViews') as page_view,
-json_extract_scalar(trafficByAsin,'$.sessions') as sessions,
-CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.orderedProductSales.amount') AS FLOAT64) AS ordered_product_sales,
-CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.orderedProductSalesB2B.amount') AS FLOAT64) AS ordered_product_sales_b2b,
-CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.totalOrderItems') AS INT64) AS total_order_items,
-CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.totalOrderItemsB2B') AS INT64) AS total_order_items_b2b
+  DISTINCT
+  _airbyte_extracted_at,
+  sku,
+  childAsin,
+  parentAsin,
+  queryEndDate,
+  JSON_EXTRACT_SCALAR(trafficByAsin, '$.browserPageViews') AS browser_page_view,
+  JSON_EXTRACT_SCALAR(trafficByAsin, '$.browserSessions') AS browser_sessions,
+  JSON_EXTRACT_SCALAR(trafficByAsin, '$.mobileAppPageViews') AS app_page_view,
+  JSON_EXTRACT_SCALAR(trafficByAsin, '$.pageViews') AS page_view,
+  JSON_EXTRACT_SCALAR(trafficByAsin, '$.sessions') AS sessions,
+  CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.orderedProductSales.amount') AS FLOAT64) AS ordered_product_sales,
+  CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.orderedProductSalesB2B.amount') AS FLOAT64) AS ordered_product_sales_b2b,
+  CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.totalOrderItems') AS INT64) AS total_order_items,
+  CAST(JSON_EXTRACT_SCALAR(salesByAsin, '$.totalOrderItemsB2B') AS INT64) AS total_order_items_b2b,
 
+  -- Composite key for uniqueness
+  CONCAT(
+    COALESCE(parentAsin, 'NULL'), '-', 
+    FORMAT_TIMESTAMP('%Y-%m-%d', queryEndDate)
+  ) AS composite_key
 
- FROM `shopify-pubsub-project.pilgrim_bi_airbyte_amazon_seller.GET_SALES_AND_TRAFFIC_REPORT` 
+FROM 
+  `shopify-pubsub-project.pilgrim_bi_airbyte_amazon_seller.GET_SALES_AND_TRAFFIC_REPORT`;
