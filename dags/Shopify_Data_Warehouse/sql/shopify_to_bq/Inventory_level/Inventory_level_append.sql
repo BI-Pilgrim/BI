b24c44e5dd5989209cf114fa0ab8efa15040bd49
KajalRay-12 <kajal.ray@discoverpilgrim.com>
2025-01-16 12:48:13 +0530

    Update Inventory_level_append.sql

--- dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Inventory_level/Inventory_level_append.sql
+++ dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Inventory_level/Inventory_level_append.sql
@@ -2,32 +2,40 @@
 MERGE INTO `shopify-pubsub-project.Data_Warehouse_Shopify_Staging.Inventory_level` AS target  
 USING (
 SELECT 
-distinct
-_airbyte_extracted_at,
-id as inventory_level_id,
-shop_url,
-created_at as inventory_created_at,
-updated_at as inventory_updated_at,
-REGEXP_EXTRACT(JSON_EXTRACT_SCALAR(item, '$.admin_graphql_api_id'), r'inventory_item_id=([0-9]+)') AS item_id,
-JSON_EXTRACT_SCALAR(item, '$.name') AS inventory_status,
-CAST(JSON_EXTRACT_SCALAR(item, '$.quantity') AS INT64) AS inventory_quantity,
-CAST(JSON_EXTRACT_SCALAR(item, '$.updatedAt') AS TIMESTAMP)AS status_updated_at,
-location_id as inventory_location_id,
-can_deactivate,
-CAST(json_extract_scalar(locations_count,"$.count") as INT64) as inventory_location_count,
-inventory_item_id,
-deactivation_alert,
-admin_graphql_api_id,
-inventory_history_url
+  DISTINCT
+  _airbyte_extracted_at,
+  id AS inventory_level_id,
+  shop_url,
+  created_at AS inventory_created_at,
+  updated_at AS inventory_updated_at,
+  REGEXP_EXTRACT(JSON_EXTRACT_SCALAR(item, '$.admin_graphql_api_id'), r'inventory_item_id=([0-9]+)') AS item_id,
+  JSON_EXTRACT_SCALAR(item, '$.name') AS inventory_status,
+  CAST(JSON_EXTRACT_SCALAR(item, '$.quantity') AS INT64) AS inventory_quantity,
+  CAST(JSON_EXTRACT_SCALAR(item, '$.updatedAt') AS TIMESTAMP) AS status_updated_at,
+  location_id AS inventory_location_id,
+  can_deactivate,
+  CAST(JSON_EXTRACT_SCALAR(locations_count, "$.count") AS INT64) AS inventory_location_count,
+  inventory_item_id,
+  deactivation_alert,
+  admin_graphql_api_id,
+  inventory_history_url,
+
+  -- Composite key for uniqueness
+  CONCAT(
+    CAST(id AS STRING), '-', 
+    COALESCE(CAST(REGEXP_EXTRACT(JSON_EXTRACT_SCALAR(item, '$.admin_graphql_api_id'), r'inventory_item_id=([0-9]+)') AS STRING), 'NULL'), '-', 
+    CAST(location_id AS STRING), '-', 
+    FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', created_at)
+  ) AS composite_key
 
 FROM
-   `shopify-pubsub-project.pilgrim_bi_airbyte.inventory_levels`,
+  `shopify-pubsub-project.pilgrim_bi_airbyte.inventory_levels`,
   UNNEST(JSON_EXTRACT_ARRAY(quantities)) AS item
 
 WHERE DATE(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 10 DAY)
 ) AS source
 
-ON target.inventory_level_id = source.inventory_level_id
+ON target.composite_key = source.composite_key
 
 WHEN MATCHED AND source._airbyte_extracted_at > target._airbyte_extracted_at 
 THEN UPDATE SET
@@ -47,7 +55,8 @@ target.inventory_location_count = source.inventory_location_count,
 target.inventory_item_id = source.inventory_item_id,
 target.deactivation_alert = source.deactivation_alert,
 target.admin_graphql_api_id = source.admin_graphql_api_id,
-target.inventory_history_url = source.inventory_history_url
+target.inventory_history_url = source.inventory_history_url,
+target.composite_key = source.composite_key
 
 
 WHEN NOT MATCHED THEN INSERT (
@@ -66,7 +75,8 @@ inventory_location_count,
 inventory_item_id,
 deactivation_alert,
 admin_graphql_api_id,
-inventory_history_url
+inventory_history_url,
+composite_key
   
   )
   VALUES (
@@ -85,5 +95,6 @@ inventory_history_url
   source.inventory_item_id,
   source.deactivation_alert,
   source.admin_graphql_api_id,
-  source.inventory_history_url
+  source.inventory_history_url,
+  source.composite_key
   );
