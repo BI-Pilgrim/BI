09c9dccd54013b6af1bc788974a568d8c9543a38
KajalRay-12 <kajal.ray@discoverpilgrim.com>
2025-01-17 16:52:29 +0530

    Update Order_items_append.sql

--- dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Order Items/Order_items_append.sql
+++ dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Order Items/Order_items_append.sql
@@ -21,21 +21,18 @@ JSON_EXTRACT_SCALAR(item, '$.sku') AS item_sku_code,
 CAST(JSON_EXTRACT_SCALAR(item, '$.total_discount') AS FLOAT64) AS item_discount,
 CAST(JSON_EXTRACT_SCALAR(item, '$.variant_id') AS INT64) AS item_variant_id,
 JSON_EXTRACT_SCALAR(item, '$.fulfillment_status') AS item_fulfillment_status
-FROM
- `shopify-pubsub-project.pilgrim_bi_airbyte.orders`,
-  UNNEST(JSON_EXTRACT_ARRAY(line_items)) AS item;
 updated_at as Order_updated_at,
 created_at as Order_created_at,
 CAST(processed_at as TIMESTAMP) as Order_processed_at,
 JSON_EXTRACT_SCALAR(customer, '$.id') AS customer_id,
 CAST(JSON_EXTRACT_SCALAR(tax_lines, '$[0].price') AS FLOAT64) AS order_tax_price,
 JSON_EXTRACT_SCALAR(item, '$.name') AS item_name,
-  CAST(JSON_EXTRACT_SCALAR(item, '$.price') AS FLOAT64) AS item_price,
-  CAST(JSON_EXTRACT_SCALAR(item, '$.quantity') AS INT64) AS item_quantity,
-  JSON_EXTRACT_SCALAR(item, '$.sku') AS item_sku_code,
-  CAST(JSON_EXTRACT_SCALAR(item, '$.total_discount') AS FLOAT64) AS item_discount,
-  CAST(JSON_EXTRACT_SCALAR(item, '$.variant_id') AS INT64) AS item_variant_id,
-  JSON_EXTRACT_SCALAR(item, '$.fulfillment_status') AS item_fulfillment_status
+CAST(JSON_EXTRACT_SCALAR(item, '$.price') AS FLOAT64) AS item_price,
+CAST(JSON_EXTRACT_SCALAR(item, '$.quantity') AS INT64) AS item_quantity,
+JSON_EXTRACT_SCALAR(item, '$.sku') AS item_sku_code,
+CAST(JSON_EXTRACT_SCALAR(item, '$.total_discount') AS FLOAT64) AS item_discount,
+CAST(JSON_EXTRACT_SCALAR(item, '$.variant_id') AS INT64) AS item_variant_id,
+JSON_EXTRACT_SCALAR(item, '$.fulfillment_status') AS item_fulfillment_status
 FROM
  `shopify-pubsub-project.pilgrim_bi_airbyte.orders`,
   UNNEST(JSON_EXTRACT_ARRAY(line_items)) AS item
