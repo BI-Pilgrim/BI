0d9b00fa95f357d2b5f9ecf077baae86c38e30fa
PilgrimBI <cloud@discoverpilgrim.com>
2025-01-20 14:44:18 +0530

    Update Amazon_Seller_DW_dag.py

--- dags/Amazon_Seller_Data_Warehouse/dag/Amazon_Seller_DW_dag.py
+++ dags/Amazon_Seller_Data_Warehouse/dag/Amazon_Seller_DW_dag.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.utils.dates import days_ago, timezone
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCheckOperator, BigQueryInsertJobOperator
 
 # Define the start date in UTC 
