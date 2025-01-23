16fcb26d763963ae9faf702354e455b51a80f8da
Omkar0893 <omkar.sadawarte@discoverpilgrim.com>
2025-01-23 15:50:30 +0530

    update facebook_ads_daily_log

--- dags/Facebook_Ads_Performance_Report/SQL/facebook_ads_daily_log/facebook_ads_daily_log_append.sql
+++ dags/Facebook_Ads_Performance_Report/SQL/facebook_ads_daily_log/facebook_ads_daily_log_append.sql
@@ -1,9 +1,13 @@
-CREATE OR REPLACE TABLE `shopify-pubsub-project.adhoc_data_asia.facebook_ads_daily_log`
-AS
+-- Remove entries for the current date to avoid duplicates
+DELETE FROM `shopify-pubsub-project.adhoc_data_asia.facebook_ads_daily_log`
+WHERE run_date = CURRENT_DATE("Asia/Kolkata");
+
+-- Insert new data for the current date
+INSERT INTO `shopify-pubsub-project.adhoc_data_asia.facebook_ads_daily_log`
 SELECT
   CURRENT_DATE("Asia/Kolkata") AS run_date,
   COUNT(DISTINCT A.ad_id) AS ad_count,
-  B.TIER,
+  B.TIER
 FROM `shopify-pubsub-project.adhoc_data_asia.daily_ad_spend_and_revenue` A
 JOIN `shopify-pubsub-project.adhoc_data_asia.FACEBOOK_ADS_SPEND_TIERS_NEW` B
 ON A.ad_id = B.ad_id
@@ -11,13 +15,8 @@ GROUP BY B.TIER
 
 UNION ALL
 
-
 SELECT
   CURRENT_DATE("Asia/Kolkata") AS run_date,
   COUNT(DISTINCT ad_id) AS ad_count,
   'TOTAL AD COUNT' AS TIER
-FROM `shopify-pubsub-project.adhoc_data_asia.daily_ad_spend_and_revenue` 
-
-
-
-
+FROM `shopify-pubsub-project.adhoc_data_asia.daily_ad_spend_and_revenue`;
