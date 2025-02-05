6a3b41d7446d31907552440840bd8ce74997bed8
shafiq115 <shafiq@discoverpilgrim.com>
2025-02-05 15:20:36 +0530

    Update Metafield_customer_append.sql

--- dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Metafield_Customer/Metafield_customer_append.sql
+++ dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Metafield_Customer/Metafield_customer_append.sql
@@ -13,6 +13,11 @@ min(_airbyte_extracted_at) as _airbyte_extracted_at,
 COALESCE(MAX(case when key='gender' then value end),'NIL') as Gender_field,
 COALESCE(MAX(case when key='personalization_products' then value end),'NIL') as Personalization_field,
 COALESCE(MAX(case when key='concerns' then value end),'NIL') as Concerns_field,
+COALESCE(MAX(case when key='profession' then value end),'NIL') as Profession_field,
+COALESCE(MAX(case when key='expected_gender' then value end),'NIL') as Gender_der_field,
+COALESCE(MAX(case when key='dob' then value end),'NIL') as DOB_field,
+COALESCE(MAX(case when key='language' then value end),'NIL') as Language_field,
+COALESCE(MAX(case when key='in_love_with' then value end),'NIL') as loved_product_field
 --admin_graphql_api_id as admin_graphql_api_id
 
  FROM  `shopify-pubsub-project.pilgrim_bi_airbyte.metafield_customers`
@@ -31,7 +36,12 @@ target.customer_created_at = source.customer_created_at,
 target.customer_updated_at = source.customer_updated_at,
 target.Gender_field = source.Gender_field,
 target.Personalization_field = source.Personalization_field,
-target.Concerns_field = source.Concerns_field
+target.Concerns_field = source.Concerns_field,
+target.Profession_field = source.Profession_field,
+target.Gender_der_field = source.Gender_der_field,
+target.DOB_field = source.DOB_field,
+target.Language_field = source.Language_field,
+target.loved_product_field = source.loved_product_field
 --target.admin_graphql_api_id = source.admin_graphql_api_id
 
 WHEN NOT MATCHED THEN INSERT (
@@ -44,7 +54,12 @@ customer_created_at,
 customer_updated_at,
 Gender_field,
 Personalization_field,
-Concerns_field
+Concerns_field,
+Profession_field,
+Gender_der_field,
+DOB_field,
+Language_field,
+loved_product_field
 --admin_graphql_api_id
 
    )
@@ -57,9 +72,11 @@ source.customer_created_at,
 source.customer_updated_at,
 source.Gender_field,
 source.Personalization_field,
-source.Concerns_field
+source.Concerns_field,
+source.Profession_field,
+source.Gender_der_field,
+source.DOB_field,
+source.Language_field,
+source.loved_product_field
 --source.admin_graphql_api_id 
   )
-
-
-
