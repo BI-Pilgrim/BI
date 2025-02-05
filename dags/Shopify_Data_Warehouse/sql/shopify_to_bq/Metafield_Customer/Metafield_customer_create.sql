a677591999a3cdd3abd791eb84b1a8f9e3b6cf86
shafiq115 <shafiq@discoverpilgrim.com>
2025-02-05 15:18:48 +0530

    Added  more metafiled in source

--- dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Metafield_Customer/Metafield_customer_create.sql
+++ dags/Shopify_Data_Warehouse/sql/shopify_to_bq/Metafield_Customer/Metafield_customer_create.sql
@@ -16,7 +16,14 @@ Max(updated_at) as customer_updated_at,
 min(_airbyte_extracted_at) as _airbyte_extracted_at,
 COALESCE(MAX(case when key='gender' then value end),'NIL') as Gender_field,
 COALESCE(MAX(case when key='personalization_products' then value end),'NIL') as Personalization_field,
-COALESCE(MAX(case when key='concerns' then value end),'NIL') as Concerns_field
+COALESCE(MAX(case when key='concerns' then value end),'NIL') as Concerns_field,
+COALESCE(MAX(case when key='profession' then value end),'NIL') as Profession_field,
+COALESCE(MAX(case when key='expected_gender' then value end),'NIL') as Gender_der_field,
+COALESCE(MAX(case when key='dob' then value end),'NIL') as DOB_field,
+COALESCE(MAX(case when key='language' then value end),'NIL') as Language_field,
+COALESCE(MAX(case when key='in_love_with' then value end),'NIL') as loved_product_field,
+
+
 --admin_graphql_api_id as admin_graphql_api_id
 
  FROM  `shopify-pubsub-project.pilgrim_bi_airbyte.metafield_customers`
