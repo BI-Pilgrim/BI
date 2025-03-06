0dc6d917edc1dfc90040bc0f292d4ac0f89a7280
aryanpil <aryan.kumar@discoverpilgrim.com>
2025-03-06 17:41:26 +0530

    Update Clickpost_DW_Dag.py

--- dags/Clickpost_Data_Warehouse/dag/Clickpost_DW_Dag.py
+++ dags/Clickpost_Data_Warehouse/dag/Clickpost_DW_Dag.py
@@ -44,32 +44,32 @@ with DAG(
             } 
         ) 
     
-    with open('../dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/orders/Clickpost_orders_Append.sql', 'r') as file: 
+    with open('/home/airflow/gcs/dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/orders/Clickpost_orders_Append.sql', 'r') as file: 
         sql_query_1 = file.read() 
     
     task_query_1 = create_query_task(sql_query_1, 'orders_Append') 
 
-    with open('../dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/addresses/Clickpost_addresses_Append.sql', 'r') as file: 
+    with open('/home/airflow/gcs/dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/addresses/Clickpost_addresses_Append.sql', 'r') as file: 
         sql_query_2 = file.read() 
 
     task_query_2 = create_query_task(sql_query_2, 'addresses_Append') 
 
-    with open('../dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/package/Clickpost_package_Append.sql', 'r') as file: 
+    with open('/home/airflow/gcs/dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/package/Clickpost_package_Append.sql', 'r') as file: 
         sql_query_3 = file.read() 
 
     task_query_3 = create_query_task(sql_query_3, 'package_Append') 
 
-    with open('../dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/shipping/Clickpost_shipping_Append.sql', 'r') as file: 
+    with open('/home/airflow/gcs/dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/shipping/Clickpost_shipping_Append.sql', 'r') as file: 
         sql_query_4 = file.read() 
 
     task_query_4 = create_query_task(sql_query_4, 'shipping_Append') 
 
-    with open('../dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/tracking/Clickpost_tracking_Append.sql', 'r') as file: 
+    with open('/home/airflow/gcs/dags/Clickpost_Data_Warehouse/sql/clickpost_to_bq/tracking/Clickpost_tracking_Append.sql', 'r') as file: 
         sql_query_5 = file.read() 
 
     task_query_5 = create_query_task(sql_query_5, 'tracking_Append') 
 
-    with open('../dags/Clickpost_Data_Warehouse/sql/Clickpost_DW_sanity_check.sql', 'r') as file: 
+    with open('/home/airflow/gcs/dags/Clickpost_Data_Warehouse/sql/Clickpost_DW_sanity_check.sql', 'r') as file: 
         sql_query_6 = file.read() 
 
     task_query_6 = create_query_task(sql_query_6, 'sanity_check') 
