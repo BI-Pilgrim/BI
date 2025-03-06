a2973d9f25213983fb4931c5243799a793139897
Omkar0893 <omkar.sadawarte@discoverpilgrim.com>
2025-03-06 14:22:10 +0530

    Update ad_group_ad_vid_resp_ad_call_to_actions_append.sql

--- dags/Google_Ads_Warehouse/sql/GoogleAds_to_bq/ad_group_ad_vid_resp_ad_call_to_actions/ad_group_ad_vid_resp_ad_call_to_actions_append.sql
+++ dags/Google_Ads_Warehouse/sql/GoogleAds_to_bq/ad_group_ad_vid_resp_ad_call_to_actions/ad_group_ad_vid_resp_ad_call_to_actions_append.sql
@@ -14,7 +14,9 @@ row_number() over(partition by ad_group_ad_ad_id,segments_date,REGEXP_EXTRACT(JS
 from `shopify-pubsub-project.pilgrim_bi_google_ads.ad_group_ad`,
   UNNEST(JSON_EXTRACT_ARRAY(ad_group_ad_ad_video_responsive_ad_call_to_actions)) AS action
 )
-where rn = 1 and DATE(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 10 DAY)
+where rn = 1 
+and REGEXP_EXTRACT(JSON_EXTRACT_SCALAR(action, '$'), r'text: \"([^\"]+)\"') is not null
+and DATE(_airbyte_extracted_at) >= DATE_SUB(CURRENT_DATE("Asia/Kolkata"), INTERVAL 10 DAY)
 ) AS SOURCE
 ON TARGET.ad_group_ad_ad_id = SOURCE.ad_group_ad_ad_id
 and TARGET.segments_date = SOURCE.segments_date
